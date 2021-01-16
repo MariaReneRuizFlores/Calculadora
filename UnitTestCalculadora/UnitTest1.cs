47e029631f92baa36d579275859a790dad1dabeb
María René Ruiz Flores <a2019117746@estudiantes.upsa.edu.bo>
2021-01-15 21:46:27 -0400

    Se hizo 3UT para resta (Get) y 3UT para restar (Post)

--- UnitTestCalculadora/UnitTest1.cs
+++ UnitTestCalculadora/UnitTest1.cs
@@ -27,9 +27,9 @@ namespace UnitTestCalculadora
         public void TestSuma2()
         {
             //Arrange=Preparación
-            int num1 = 5;
-            int num2 = 7;
-            int esperado = 12;
+            int num1 = 8;
+            int num2 = 9;
+            int esperado = 17;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.suma(num1, num2);
@@ -41,9 +41,9 @@ namespace UnitTestCalculadora
         public void TestSuma3()
         {
             //Arrange=Preparación
-            int num1 = 5;
-            int num2 = 7;
-            int esperado = 12;
+            int num1 = 3;
+            int num2 = 2;
+            int esperado = 5;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.suma(num1, num2);
@@ -55,9 +55,9 @@ namespace UnitTestCalculadora
         public void TestSumar1()
         {
             //Arrange=Preparación
-            int num1 = 5;
+            int num1 = 3;
             int num2 = 7;
-            int esperado = 12;
+            int esperado = 10;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.sumar(num1, num2);
@@ -69,9 +69,9 @@ namespace UnitTestCalculadora
         public void TestSumar2()
         {
             //Arrange=Preparación
-            int num1 = 5;
+            int num1 = 14;
             int num2 = 7;
-            int esperado = 12;
+            int esperado = 21;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.sumar(num1, num2);
@@ -84,8 +84,8 @@ namespace UnitTestCalculadora
         {
             //Arrange=Preparación
             int num1 = 5;
-            int num2 = 7;
-            int esperado = 12;
+            int num2 = 2;
+            int esperado = 7;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.sumar(num1, num2);
@@ -94,12 +94,12 @@ namespace UnitTestCalculadora
             Assert.AreEqual(esperado, resultado);
         }
         [Test]
-        public void TestResta()
+        public void TestResta1()
         {
             //Arrange=Preparación
-            int num1 = 13;
+            int num1 = 19;
             int num2 = 4;
-            int esperado = 9;
+            int esperado = 15;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.resta(num1, num2);
@@ -108,12 +108,68 @@ namespace UnitTestCalculadora
             Assert.AreEqual(esperado, resultado);
         }
         [Test]
-        public void TestRestar()
+        public void TestResta2()
+        {
+            //Arrange=Preparación
+            int num1 = 15;
+            int num2 = 20;
+            int esperado = -5;
+            CalculadoraController calt = new CalculadoraController();
+            //Act=ejecución
+            int resultado = calt.resta(num1, num2);
+
+            //Assert=verificación
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestResta3()
+        {
+            //Arrange=Preparación
+            int num1 = 10;
+            int num2 = 2;
+            int esperado = 8;
+            CalculadoraController calt = new CalculadoraController();
+            //Act=ejecución
+            int resultado = calt.resta(num1, num2);
+
+            //Assert=verificación
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestRestar1()
         {
             //Arrange=Preparación
             int num1 = 22;
             int num2 = 5;
             int esperado = 17;
+            CalculadoraController calt = new CalculadoraController();
+            //Act=ejecución
+            int resultado = calt.restar(num1, num2);
+
+            //Assert=verificación
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestRestar2()
+        {
+            //Arrange=Preparación
+            int num1 = 2;
+            int num2 = 15;
+            int esperado = -13;
+            CalculadoraController calt = new CalculadoraController();
+            //Act=ejecución
+            int resultado = calt.restar(num1, num2);
+
+            //Assert=verificación
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestRestar3()
+        {
+            //Arrange=Preparación
+            int num1 = 8;
+            int num2 = 4;
+            int esperado = 4;
             CalculadoraController calt = new CalculadoraController();
             //Act=ejecución
             int resultado = calt.restar(num1, num2);
